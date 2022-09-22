04c0c366649d301bffb348f9e560834645755eef
kamilmarkowski <markian99@wp.pl>
2022-09-22 18:43:30 +0200

    test

--- src/test/java/com/kamil/bookslibrary/controller/BookControllerTest.java
+++ src/test/java/com/kamil/bookslibrary/controller/BookControllerTest.java
@@ -1,10 +1,40 @@
 package com.kamil.bookslibrary.controller;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
-//testy integracyjne  
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
+
 class BookControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+        //given
+
+    //when
+    void shouldGetSingleBook() throws Exception {
+mvc.perform(get("/1"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("1"));
+
+        //then
+    }
+
+
 
 }
