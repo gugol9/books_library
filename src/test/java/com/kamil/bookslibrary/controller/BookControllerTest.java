a1c7c6b371e7f7852e3f7dee0b3060d7b9a71175
kamilmarkowski <markian99@wp.pl>
2022-09-22 13:01:24 +0200

    testing 1.0

---
+++ src/test/java/com/kamil/bookslibrary/controller/BookControllerTest.java
@@ -0,0 +1,10 @@
+package com.kamil.bookslibrary.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+//testy integracyjne  
+@SpringBootTest
+class BookControllerTest {
+
+}
